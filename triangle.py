6fa7d66712ed0569810943114c5ebd3aaa01c853
XiaYue <44867741+XiaYue1013@users.noreply.github.com>
2023-11-22 00:39:09 +0800

    Create triangle.py

---
+++ triangle.py
@@ -0,0 +1,18 @@
+def t(a, b, c):
+    if a + b <= c or a + c <= b or b + c <= a:
+        return "不是有效的三角形"
+
+    if c**2 == a**2 + b**2:
+        t_type = "直角三角形"
+    elif c**2 > a**2 + b**2:
+        t_type = "鈍角三角形"
+    else:
+        t_type = "銳角三角形"
+
+    s = (a + b + c) / 2 
+    area = (s * (s - a) * (s - b) * (s - c)) ** 0.5
+
+    return f'類型: {t_type}, 面積: {area}'
+
+a, b, c = map(int, input("請輸入 a, b, c: ").split(", "))
+print(t(a, b, c))

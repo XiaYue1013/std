90ea168c835b3b60adb904baeb9a8829638a81dd
XiaYue <44867741+XiaYue1013@users.noreply.github.com>
2023-11-27 01:43:28 +0800

    Update collinear.py

--- collinear.py
+++ collinear.py
@@ -3,9 +3,9 @@ def c(x1, y1, x2, y2, x3, y3):
     area = abs((x1*(y2-y3) + x2*(y3-y1) + x3*(y1-y2)) / 2.0)
     
     if area == 0:
-        return "共線", ""
+        return "共線"
     else:
-        return "未共線，面積: ", f"{area}"
+        return f"未共線，面積: {area}"
 
-x1, y1, x2, y2, x3, y3 = map(int, input("請輸入 x1, y1, x2, y2, x3, y3: ").split(", "))
-print(c(x1, y1, x2, y2, x3, y3)[0]+c(x1, y1, x2, y2, x3, y3)[1])
+x1, y1, x2, y2, x3, y3 = map(int, input("請輸入三個座標 (x1, y1, x2, y2, x3, y3): ").split(", "))
+print(c(x1, y1, x2, y2, x3, y3))
